78962f7cf09c575df3876b7eb9da99a51f899619
Loredan <loredan725@gmail.com>
2013-08-02 17:48:20 +0400

    Fix: exclude identical coordinates during location update

--- src/com/RoboMobo/Player.java
+++ src/com/RoboMobo/Player.java
@@ -24,10 +24,13 @@ public class Player
 
     public void changePos(int[] coord)
     {
-        this.prevPosX = this.posX;
-        this.prevPosY = this.posY;
-        posX = coord[0];
-        posY = coord[1];
+        if ((posX != coord[0]) || (posY != coord[1]))
+        {
+            prevPosX = posX;
+            prevPosY = posY;
+            posX = coord[0];
+            posY = coord[1];
+        }
         //Log.wtf("player","x: "+posX+", y: "+posY);
     }
 

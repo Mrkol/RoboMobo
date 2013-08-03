bfa9202f3fb94426845005b3d8ccd3226730774a
Loredan <loredan725@gmail.com>
2013-08-03 12:31:16 +0400

    New: networking class for data exchange with server (not completed)

---
+++ src/com/RoboMobo/Networking.java
@@ -0,0 +1,62 @@
+package com.RoboMobo;
+
+import android.util.Log;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: loredan
+ * Date: 03.08.13
+ * Time: 10:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class Networking
+{
+    public static HttpURLConnection connection;
+    public static URL url;
+    public static InputStream in;
+    public static OutputStream out;
+
+    public static void init(String _url)
+    {
+        try
+        {
+            url = new URL(_url);
+            connection = (HttpURLConnection) url.openConnection();
+        }
+        catch (MalformedURLException e)
+        {
+            Log.wtf("URL", e.getMessage());
+        }
+        catch (IOException e)
+        {
+            Log.wtf("IO", e.getMessage());
+        }
+    }
+
+    public static JSONObject get(JSONObject send)
+    {
+        try
+        {
+            InputStream is = connection.getInputStream();
+            OutputStream os = connection.getOutputStream();
+            os.write(send.toString().getBytes());
+            while (is.available()==0);
+            return send;
+
+        } catch (IOException e)
+        {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return null;
+    }
+}
